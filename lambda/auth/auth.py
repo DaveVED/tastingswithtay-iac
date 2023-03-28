8f239aca6b43d8ae82158d3ed5c5697f012274d5
Dave Dennis <dave.w.dennis@gmail.com>
2023-03-28 17:42:23 -0500

    working on api

--- lambda/auth/auth.py
+++ lambda/auth/auth.py
@@ -1,2 +1,9 @@
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
 def handler(event, context):
+    logger.info(f" EVENT: {event}")
     return {"statusCode": 200, "Body": "Hello World"}
