ed253bb84e9a76f16f2e2fc41324c0ac355a1cb6
Dave Dennis <dave.w.dennis@gmail.com>
2023-03-31 17:31:58 -0500

    api gw

--- lib/create_api_keys.py
+++ lib/create_api_keys.py
@@ -12,16 +12,19 @@ logger.setLevel(logging.INFO)
 client = boto3.client("secretsmanager", region_name="us-east-1")
 
 
-def generate_api_key(client: str, length=64) -> str:
+def generate_api_key(name: str, length=64) -> str:
     """Generate a secure API key with the specified length."""
     alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     api_key = "".join(secrets.choice(alphabet) for i in range(length))
-    return f"{client}_{api_key}"
+    return f"{name}_{api_key}"
 
 
-def create_secert(api_key: str):
+def manage_secert(name: str, api_key: str):
     try:
-        pass
+        response = client.create_secert(
+            "Name": name,
+            "Description": f"API Key for {name}"
+        )
     except Exception as e:
         logger.error(f"error: {e}")
         raise
@@ -29,12 +32,12 @@ def create_secert(api_key: str):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--client", required=True, help="Name of the client")
+    parser.add_argument("--name", required=True, help="Name of the client")
     args = parser.parse_args()
 
     try:
-        api_key = generate_api_key(args.client)
-        create_secert(api_key)
+        api_key = generate_api_key(args.name)
+        manage_secert(client, api_key)
     except Exception as e:
         logger.info(f"error {e}")
         sys.exit(0)
