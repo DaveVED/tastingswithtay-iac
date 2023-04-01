80a24544c4b2d79e12e18cb6d4e68e1649c5cc21
Dave Dennis <dave.w.dennis@gmail.com>
2023-04-01 12:48:34 -0500

    v6

--- lambda/auth/auth.py
+++ lambda/auth/auth.py
@@ -4,5 +4,25 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
+def generate_policy(principal_id, effect, resource):
+    return {
+        "principalId": principal_id,
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": "*",
+                    "Effect": effect,
+                    "Resource": resource,
+                }
+            ],
+        },
+    }
+
+
 def handler(event, context):
-    return {"statusCode": 200, "headers": {}, "body": "Hello World"}
+    principal_id = "*"
+    effect = "Allow"
+    resource = "*"
+
+    return generate_policy(principal_id, effect, resource)
